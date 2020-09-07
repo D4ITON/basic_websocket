678dfee2d44f76dbd3a74d93f632896d733562a4
D4ITON <daitonmh100@gmail.com>
2020-09-06 22:19:15 -0500

    Muestra pantalla de carga, y funciona traida de datos luego de agregar nuevo mensaje

--- server/src/controllers/messages.controller.js
+++ server/src/controllers/messages.controller.js
@@ -1,19 +1,31 @@
 const Chat = require("../models/Chat");
 
-async function sendDataToClient(message, io) {
+async function sendDataToClient(io) {
+  try {
+    const messages = await Chat.find({});
+    // console.log(messages);
+    io.emit("load_messages", messages);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function setNewMessage(message, io) {
   try {
     const chat = new Chat({ message });
-    chat.save((err, doc) => {
-      if (err) return console.log(err);
-      return io.emit("output_from_server", doc);
-    });
+    const messageStored = await chat.save();
+    if (messageStored) sendDataToClient(io);
   } catch (err) {
     console.log(err);
   }
 }
 
 module.exports = function (socket, io) {
-  socket.on("initial_data", function (message) {
-    sendDataToClient(message, io);
+  socket.on("set_message", function (message) {
+    setNewMessage(message, io);
+  });
+
+  socket.on("load_initial_data", function () {
+    sendDataToClient(io);
   });
 };
